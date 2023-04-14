3bd32fb0ae0c6b5bff71ae79a74dd1dfa677b6be
Dylan Govender <dylan.govender@standardbank.co.za>
2023-04-14 13:01:25 +0200

    Got sublinks from Main Page

--- src/main/java/com/dg/hpd/hpd/HpdApplication.java
+++ src/main/java/com/dg/hpd/hpd/HpdApplication.java
@@ -1,13 +1,37 @@
 package com.dg.hpd.hpd;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-@SpringBootApplication
+import java.util.ArrayList;
+
 public class HpdApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HpdApplication.class, args);
+		Document doc = getPage();
+		ArrayList<String> links = getMainPageLinks(doc);
+		System.out.println(links);
 	}
 
+	public static ArrayList<String> getMainPageLinks(Document doc) {
+		ArrayList<String> strLinks = new ArrayList<>();
+		Elements eleLinks = doc.select("a[href^='https://www.hirschs.co.za/specials-and-promotions/inactive/clearance-sale/clearance-sale-in-store/']");
+		for (Element link : eleLinks) {
+			strLinks.add(link.absUrl("href"));
+		}
+		return strLinks;
+	}
+
+	public static Document getPage() {
+		String url = "https://www.hirschs.co.za/specials-and-promotions/clearance-sale";
+		Document doc = null;
+		try{
+			doc = Jsoup.connect(url).get();
+		} catch (Exception e) {
+			System.out.println("Error getting page");
+		}
+		return doc;
+	}
 }

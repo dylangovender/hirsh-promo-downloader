bbaacaf71d6bea88978a7732c7d4186a8dff9308
Dylan Govender <dylan.govender@gmail.com>
2023-04-14 12:33:09 +0200

    Initial commit

---
+++ src/main/java/com/dg/hpd/hpd/HpdApplication.java
@@ -0,0 +1,13 @@
+package com.dg.hpd.hpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dg/hpd/hpd/HpdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg.hpd.hpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
